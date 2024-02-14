beead2a2c9aa53ba0408e1dc9f795e3d6988b822
Mochalka <bdauren0408@gmail.com>
2024-02-14 16:10:50 +0600

    Dauren (new html "Profile")

--- public/js/main.js
+++ public/js/main.js
@@ -1,18 +1,20 @@
 // codefree/public/js/main.js
 
-// Initialize the Ace editor
 var editor = ace.edit("editor");
 editor.setTheme("ace/theme/monokai");
 editor.session.setMode("ace/mode/java");
 
+
+function goToProfile() {
+    window.location.href = "/profile";
+}
+
 async function runCode() {
     var code = editor.getValue();
 
-    // Clear previous output
     document.getElementById("output").textContent = "";
 
     try {
-        // Use JDoodle API to run Java code
         var result = await executeJavaCode(code);
         console.log(result);
         document.getElementById("output").textContent = result.output;
@@ -22,22 +24,17 @@ async function runCode() {
     }
 }
 
-// Function to execute Java code using JDoodle API
 async function executeJavaCode(code) {
-    // Replace 'YOUR_JDOODLE_API_KEY' with your actual JDoodle API key
     var apiKey = 'YOUR_JDOODLE_API_KEY';
     var apiUrl = 'https://api.jdoodle.com/v1/execute';
-
-    // Prepare the request payload
     var requestData = {
         script: code,
         language: 'java',
         versionIndex: '3',
-        clientId: 'client_id',  // Use any client ID
-        clientSecret: 'client_secret'  // Use any client secret
+        clientId: '8a23b5056b9cd6d66e189ffea9dd5424',  // Use any client ID
+        clientSecret: 'd42e454b81adde23e731e323eea4f0ebda249b71e7df1c6adf1c1cad36037b35'  // Use any client secret
     };
 
-    // Send a POST request to JDoodle API
     var response = await fetch(apiUrl, {
         method: 'POST',
         headers: {
@@ -55,6 +52,3 @@ async function executeJavaCode(code) {
 
     return data;
 }
-
-
-
